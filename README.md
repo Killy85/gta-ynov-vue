eed09925e275a1f474efe71582c32484068d501b
Killy Gourou <goureau.n.a@hotmail.fr>
2018-10-26 11:33:22 +0200

    Adding base form to employee vue. Updating ReadMe about WIP and goals

--- README.md
+++ README.md
@@ -6,15 +6,26 @@
 
 As asked by the speaker running the lesson, this section will be providing prevision en feedback on work done for each session
 
-### 26 October 2018 
+### 26 October 2018
+
 #### TODO
 
     As the application we were asked for is quite big, I will get to it a way I'm not used to : I'll start with the front !
     Today, my goal is to setup a new Vue.js application, using boostrap-vue and to create an interface I may feed with the
-    large amount of data we need to display
+    large amount of data we need to display. I will focus on the employee interface and the differents tabs we will need.
 
-### What I've done?
-    
+### What I've done '?'
+
+    The main structure is here. I've got a Vue.js project running and webpack building my app correctly. I did begin the interface for the employee personna. This is really basic and need more thinking and refinment.
+
+    The problem here is that there is a lot of functionnalities to add to one interface. Next session work may be about filter functionnalities I feel I'm able to implements, and add values loading to this interface (loads user profile from json)
+
+    First time with webpack, and thanks to Vue.js it wasn't so hard. A lot of informations are already provided by the *vue-cli* template.
+
+## Access *state-of-the-art* application
+
+The project is builded and pushed to [my github.io page](https://killy85.github.io/#/)
+
 
 ## Build Setup
 
--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -25,7 +25,40 @@
     </b-navbar>
     <b-tabs>
       <b-tab title="Home" active>
-        <br>I'm the first fading tab
+        <b-container class="bv-example-row">
+        <b-row class="text-center">
+          <b-col>
+            <b-container fluid class="p-4 bg-dark">
+              <b-row>
+                <b-col>
+                  <b-img rounded="circle" width="75" height="75" blank-color="#777" class="m-1" src="https://picsum.photos/250/250/?image=54" alt="Thumbnail" />
+                  <p class="text-light"> {{ name }}</p>
+                  <p class="text-light"> {{ job }} </p>
+                  <b-button variant="primary">
+                    Request Time Off
+                  </b-button>
+                </b-col>
+              </b-row>
+            </b-container>
+            <b-container fluid class="p-4">
+             <p> My contract: </p>
+                <p>  Started at {{ beginDate }} </p>
+                <p v-if="endDate">  Finish at {{ endDate }} </p>
+                <p>  {{ hoursAWeek}} hours per week </p>
+            </b-container>
+          </b-col>
+          <b-col cols="12" lg="8">
+            <b-container fluid class="p-4">
+            <label> Name </label> 
+            <b-form-input v-model="form.name"></b-form-input>
+            <label> Job </label> 
+            <b-form-input v-model="form.job"></b-form-input>
+
+            <p>Your infos : {{ form.name }} {{ form.job }}</p>
+            </b-container>
+          </b-col>
+        </b-row>
+        </b-container>
       </b-tab>
       <b-tab title="My info">
         <br>I'm the second tab content
@@ -40,12 +73,36 @@
 
 import Calendar from '@/components/Calendar';
 
+function formatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
+
 export default {
   name: 'Employee',
   data() {
     return {
       app_name: 'GTA-Ynov-Vue',
       events: [],
+      name: 'Basic Name',
+      job: 'This is a job',
+      beginDate: formatDate(new Date()),
+      endDate : formatDate(new Date()),
+      hoursAWeek : 'Too Much',
+      form : {
+        name : this.name,
+        job : this.job
+      }
     };
   },
   components: { Calendar },
@@ -72,6 +129,5 @@ export default {
 
   a {
     color: #42b983;
-  }
-
+  }  
 </style>
